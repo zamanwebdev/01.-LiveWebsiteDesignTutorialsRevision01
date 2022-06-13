c1d57259584a180a2953b88fa86a64b3473eb5ea
Syed Zaman Mostafiz <zamanwebdev@gmail.com>
2022-06-13 13:19:07 +0600

    9/40 Live website design tutorials for beginners (Part-07) Revision Completed.

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Design</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	  <div class="headersection template clear">
+	  	<h2>Header</h2>
+	  </div>
+	  <div class="navsection template clear">
+	  	<ul>
+	  		<li><a href="index.php">Home</a></li>
+	  		<li><a href="#">About</a></li>
+	  		<li><a href="#">Contact</a></li>
+	  		<li><a href="#">Privacy</a></li>
+	  	</ul>
+	  </div>
+	  <div class="contentsection template clear">
+	  	<div class="maincontent clear">
+	  		<div class="samepost clear">
+	  			<h2>Our post title is here</h2>
+	  				<a href="#"><img src="images/post1.jpg" alt="post image"></a>
+	  			
+	  			<p>
+	  				Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here.Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here.
+	  			</p>
+	  			<div class="readmore clear"><a href="#">Read More</a></div>
+	  		</div>
+	  		<div class="samepost clear">
+	  			<h2>Our post title is here</h2>
+	  				<a href="#"><img src="images/post1.jpg" alt="post image"></a>
+	  			
+	  			<p>
+	  				Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here.Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here.
+	  			</p>
+	  			<div class="readmore clear"><a href="#">Read More</a></div>
+	  		</div>
+	  		<div class="samepost clear">
+	  			<h2>Our post title is here</h2>
+	  				<a href="#"><img src="images/post1.jpg" alt="post image"></a>
+	  			
+	  			<p>
+	  				Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here.Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here. Main Content text will go here.
+	  			</p>
+	  			<div class="readmore clear"><a href="#">Read More</a></div>
+	  		</div>
+	  		
+	  		
+	  	</div>
+	  	<div class="sidebar clear">
+	  		<div class="samesidebar">
+	  			<h2>Sidebar one header</h2>
+	  			<p>
+	  				This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. 
+	  			</p>	
+	  			<p>
+	  				This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. 
+	  			</p>	 		
+	  		</div>
+	  		<div class="samesidebar">
+	  			<h2>Sidebar one header</h2>
+	  			<p>
+	  				This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. 
+	  			</p>	
+	  			<p>
+	  				This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. 
+	  			</p>	 		
+	  		</div>
+	  		<div class="samesidebar">
+	  			<h2>Sidebar one header</h2>
+	  			<p>
+	  				This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. 
+	  			</p>	
+	  			<p>
+	  				This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. This is sidebar. 
+	  			</p>	 		
+	  		</div>
+	  		 
+	  	</div>
+	  </div>
+	  <div class="footersection template clear">
+	  	<P>This is Footer.</P>
+	  </div>
+
+</body>
+</html>
